f9b50b4e98b44530308f43ca87e876e301888f8c
elodeab <47059818+elodeab@users.noreply.github.com>
2023-01-18 22:32:31 +0300

    create groupAnagrams.java

---
+++ Arrays & Hashing/groupAnagrams.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        HashMap<String, List<String>> sortedChars = new HashMap<>();
+        // {aet=[eat,tea,ate] , ant=[ant,tan,nat], abt=[bat]} 
+        
+        for(String word:strs){
+
+            char[] letters = word.toCharArray();
+            Arrays.sort(letters);
+            String keyLetters = new String(letters);
+
+            if(sortedChars.containsKey(keyLetters)){
+
+                List<String> anagram_strings = sortedChars.get(keyLetters);
+                anagram_strings.add(word);
+                sortedChars.put(keyLetters, anagram_strings);
+            }
+
+            else{
+                
+                List<String> anagram_strings = new ArrayList<>(List.of(word));
+                sortedChars.put(keyLetters,anagram_strings);
+            }
+
+        }
+        
+        List<List<String>> finalList = new ArrayList<List<String>>();
+
+        for(List<String> i:sortedChars.values()){
+            finalList.add(i);
+        }
+
+        return finalList;
+
+    }
+}

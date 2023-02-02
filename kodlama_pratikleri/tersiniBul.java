d3a558b2d92c94c04181f1f8a9eb83f28e5fb670
niluzunoglu <47059818+niluzunoglu@users.noreply.github.com>
2023-02-02 20:27:55 +0300

    Update tersiniBul.java

--- kodlama_pratikleri/tersiniBul.java
+++ kodlama_pratikleri/tersiniBul.java
@@ -2,8 +2,12 @@ public class tersiniBul {
 
     public static String tersiniBul(String word,int index) {
         
-        if(index<word.length()) return tersiniBul(word, index+1) + word.charAt(index);
-        else return "";
+        if(index<word.length()) {
+            return tersiniBul(word, index+1) + word.charAt(index);
+        }
+        else {
+            return "";
+        }
         
     }
 
